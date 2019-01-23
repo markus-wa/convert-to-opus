41bc4cccfc319bc0d5026b20d9427b61fc58de80
Markus Walther <m.walther97@gmail.com>
2019-01-23 21:16:12 +0100

    use logging package for printing to stdout

--- to_opus.py
+++ to_opus.py
@@ -2,6 +2,7 @@ from subprocess import Popen
 from multiprocessing import Pool
 from shutil import copyfile, which
 from pathlib import Path
+import logging
 import sys
 import os
 
@@ -9,16 +10,24 @@ if which('opusenc') is None:
     print("ERROR: opusenc not found in PATH - please make sure it's installed",  file=sys.stderr)
     exit(1)
 
+logging.basicConfig(
+    stream=sys.stdout,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
 source = sys.argv[1]
 target = sys.argv[2]
 
 
 # We can't use a lambda since they aren't picklable
 def opusenc(src, dest):
-    if Popen(['opusenc', src, dest]).wait() is not 0:
+    if Popen(['opusenc', src, dest]).wait() is 0:
+        logging.info('converted "{0}" -> "{1}"'.format(src, dest))
+    else:
         # Fallback to copy
         # Can happen if a .ogg file is encoded as Vorbis, not FLAC
-        print('Falling back to copy for {0}'.format(src))
+        logging.warning('Falling back to copy for {0}'.format(src))
         _, src_ext = os.path.splitext(src)
         target_base, _ = os.path.splitext(dest)
         copyfile(src, target_base + src_ext)
@@ -38,7 +47,7 @@ def base_action(src_base, src_ext, dest_ext, f):
     Path(dest_base).parent.mkdir(parents=True, exist_ok=True)
     if not os.path.isfile(dest_path):
         src_path = src_base + src_ext
-        print('copy/convert from: "{0}" to: "{1}"'.format(src_path, dest_path))
+        logging.info('copy/convert: "{0}" -> "{1}"'.format(src_path, dest_path))
         pool.apply_async(f, (src_path, dest_path,))
 
 
@@ -62,6 +71,9 @@ def main():
 
 if __name__ == '__main__':
     pool = Pool(processes=8)
+    logging.info('checking for unconverted files')
     main()
+    logging.info('finishing conversions')
     pool.close()
     pool.join()
+    logging.info('conversions finished')
